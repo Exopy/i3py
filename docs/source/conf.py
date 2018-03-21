093fc87d4a9fcbae04131fef1f896e781ea55ad6
Matthieu Dartiailh <marul@laposte.net>
2018-03-21 15:14:44 +0100

    docs: fix conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'i3py'
-copyright = '2017, I3py Authors'
+copyright = '2017-2018, I3py Authors'
 author = 'I3py Authors'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -89,7 +89,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'readthedocs'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the

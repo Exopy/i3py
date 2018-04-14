d262dab5ced024aa18a386df274d676d920bb2de
Matthieu Dartiailh <marul@laposte.net>
2018-04-14 20:43:28 +0200

    core: fixes to lazy package

--- tests/core/test_lazy_package.py
+++ tests/core/test_lazy_package.py
@@ -9,6 +9,7 @@
 """Module dedicated to testing the LazyPackage.
 
 """
+import ctypes
 import concurrent
 from concurrent.futures import ThreadPoolExecutor
 import pytest
@@ -20,7 +21,7 @@ def test_getting_a_local_value():
     """Test that we can access to local values.
 
     """
-    pack = LazyPackage({}, 'test', '', {'a': 1})
+    pack = LazyPackage({}, 'concurrent', '', {'a': 1})
     assert pack.a == 1
     assert sorted(pack.__all__) == ['a']
 
@@ -30,8 +31,7 @@ def test_doing_a_lazy_import():
 
     """
     pack = LazyPackage({'ThreadPoolExecutor': 'futures.ThreadPoolExecutor'},
-                       'test', '', {'a': 1})
-    pack.__package__ = concurrent.__package__
+                       'concurrent', '', {'a': 1})
     assert pack.ThreadPoolExecutor is ThreadPoolExecutor
     assert sorted(pack.__all__) == ['ThreadPoolExecutor', 'a']
 
@@ -40,11 +40,10 @@ def test_importing_from_another_lazy_package():
     """Test that we can access the namespace of another LazyPackage.
 
     """
-    pack = LazyPackage({}, 'test', '',
+    pack = LazyPackage({}, 'ctypes', '',
                        {'test2': LazyPackage({'ThreadPoolExecutor':
                                               'futures.ThreadPoolExecutor'},
-                                             'test', '', {'a': 1})})
-    pack._local_vars['test2'].__package__ = concurrent.__package__
+                                             'concurrent', '', {'a': 1})})
     assert pack.ThreadPoolExecutor is ThreadPoolExecutor
     assert sorted(pack.__all__) == ['ThreadPoolExecutor', 'a', 'test2']
 
@@ -53,8 +52,8 @@ def test_handling_missing_attr():
     """Test that we get the proper error in a meaningless case.
 
     """
-    pack = LazyPackage({}, 'test', '',
+    pack = LazyPackage({}, 'concurrent', '',
                        {'test2': LazyPackage({'sqrt': 'math.sqrt'},
-                                             'test', '', {'a': 1})})
+                                             'ctypes', '', {'a': 1})})
     with pytest.raises(AttributeError):
         pack.b

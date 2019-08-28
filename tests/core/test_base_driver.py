08fa0584d0ffb6e155cb6cc0cf8eb642d4178f90
MatthieuDartiailh <marul@laposte.net>
2019-08-28 18:39:20 -0400

    tests: add missing test for enforcing the presence of __version__

--- tests/core/test_base_driver.py
+++ tests/core/test_base_driver.py
@@ -9,14 +9,17 @@
 """Test base driver functionalities.
 
 """
-from pytest import raises
+from pytest import raises, fixture
 
 from i3py.core.base_driver import BaseDriver
 
-BaseDriver.__version__ = '0.1.0'
+
+@fixture
+def base_version():
+    BaseDriver.__version__ = '0.1.0'
 
 
-def test_bdriver_multiple_creation():
+def test_bdriver_multiple_creation(base_version):
     a = BaseDriver(a=1)
     assert hasattr(a, 'owner') is True
     assert hasattr(a, 'lock') is True
@@ -33,26 +36,50 @@ def test_bdriver_multiple_creation():
     assert c is not b
 
 
-def test_bdriver_initiliaze():
+def test_driver_enforce_version():
+    """Test that properly validate the presence of a __version__ attr.
+
+    """
+    del BaseDriver.__version__
+    assert "__version__" not in dir(BaseDriver)
+    with raises(AttributeError) as excinfo:
+        class A(BaseDriver):
+            pass
+        A()
+
+    assert "__version__" in str(excinfo.value)
+
+    class A(BaseDriver):
+        __version__ = "0.0.1"
+
+    with raises(AttributeError) as excinfo:
+        class B(A):
+            pass
+        B()
+
+    assert "__version__" in str(excinfo.value)
+
+
+def test_bdriver_initiliaze(base_version):
     with raises(NotImplementedError):
         BaseDriver(a=1).initialize()
 
 
-def test_bdriver_finalize():
+def test_bdriver_finalize(base_version):
     with raises(NotImplementedError):
         BaseDriver(a=1).finalize()
 
 
-def test_bdriver_check():
+def test_bdriver_check(base_version):
     assert not BaseDriver(a=1).check_connection()
 
 
-def test_bdriver_connected():
+def test_bdriver_connected(base_version):
     with raises(NotImplementedError):
         BaseDriver(a=1).is_connected()
 
 
-def test_bdriver_context():
+def test_bdriver_context(base_version):
 
     class Driver(BaseDriver):
 
